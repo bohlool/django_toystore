6758251e55281b69b2e6bb23f068a52dd03b2d8c
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-02-16 22:13:09 +0330

    First commit

---
+++ django_toystore/sample_settings.py
@@ -0,0 +1,12 @@
+ADMIN_URL = ''
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-lnx534fqru*0v*7q=89g=ks66$3tr6!tu$&3+_5+^6(j9l+!4m'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
