6231a2bc3575171daee4e0fc0d65645d64b7d2b8
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-02-17 21:32:39 +0330

    Added blog admin.

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class CommentInlineAdmin(admin.TabularInline):
+    model = Comment
+    extra = 1
+    fields = ('user', 'date', 'text')
+    readonly_fields = ('date',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInlineAdmin]
+    list_display = ('id', 'title', 'subject', 'user', 'date')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'subject', 'text')
+    list_filter = ('subject', 'user', 'date')
+    date_hierarchy = 'date'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'user', 'date', 'text')
+    list_display_links = ('id', 'post')
+    search_fields = ('text',)
+    list_filter = ('user', 'date')
+    date_hierarchy = 'date'
