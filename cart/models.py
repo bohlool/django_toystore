79795655b989167e0a4fcdd4ca65b726659f4be6
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-02-23 11:49:19 +0330

    Improved cart handling.

--- cart/models.py
+++ cart/models.py
@@ -8,15 +8,28 @@ User = get_user_model()
 
 class Cart(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    is_ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return 'Cart #' + str(self.id)
+
+    @property
+    def total_price(self):
+        return sum(item.item_price for item in self.items.all())
+
+    @property
+    def total_count(self):
+        return sum(item.quantity for item in self.items.all())
 
 
 class CartItem(TimeStampedModel):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='cart_items')
-    quantity = models.IntegerField(default=1)
+    quantity = models.IntegerField(default=0)
 
     def __str__(self):
         return 'Item #' + str(self.id) + ': ' + self.product.name
+
+    @property
+    def item_price(self):
+        return self.quantity * self.product.price
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -13,37 +11,16 @@ from .models import Cart, CartItem
 
 def get_user_cart(request):
     """Retrieves the shopping cart for the current user."""
-    cart_id = None
-    cart = None
-    # If the user is logged in, then grab the user's cart info.
-    if request.user.is_authenticated and not request.user.is_anonymous:
-        try:
-            cart = Cart.objects.get(user=request.user)
-        except Cart.DoesNotExist:
-            cart = Cart(user=request.user)
-            cart.save()
-    else:
-        cart_id = request.session.get('cart_id')
-        if not cart_id:
-            cart = Cart()
-            cart.save()
-            request.session['cart_id'] = cart.id
-        else:
-            cart = Cart.objects.get(id=cart_id)
+    try:
+        cart = Cart.objects.get(user=request.user, is_ordered=False)
+    except Cart.DoesNotExist:
+        cart = Cart(user=request.user)
+        cart.save()
     return cart
 
 
-def get_cart_count(request):
-    cart = get_user_cart(request)
-    total_count = 0
-    cart_items = CartItem.objects.filter(cart=cart)
-    for item in cart_items:
-        total_count += item.quantity
-    return total_count
-
-
-def update_cart_info(request):
-    request.session['cart_count'] = get_cart_count(request)
+def update_cart_info(request, cart):
+    request.session['cart_count'] = cart.total_count
 
 
 class CartView(LoginRequiredMixin, TemplateView):
@@ -51,13 +28,7 @@ class CartView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cart = get_user_cart(self.request)
-        cart_items = CartItem.objects.filter(cart=cart)
-        order_total = Decimal(0.0)
-        for item in cart_items:
-            order_total += (item.product.price * item.quantity)
-        context['cart_items'] = cart_items
-        context['order_total'] = order_total
+        context['cart'] = get_user_cart(self.request)
         return context
 
 
@@ -71,17 +42,15 @@ class AddToCartView(LoginRequiredMixin, View):
         cart_item.quantity += quantity
         cart_item.save()
 
-        request.session['cart_count'] = request.session.get('cart_count', 0) + quantity
-        update_cart_info(request)
+        update_cart_info(request, cart)
         return HttpResponseRedirect(reverse_lazy('view-cart'))
 
 
 class RemoveFromCartView(LoginRequiredMixin, View):
     def post(self, request, item_id, *args, **kwargs):
         cart_item = get_object_or_404(CartItem, id=item_id)
-        quantity = cart_item.quantity
+        cart = cart_item.cart
         cart_item.delete()
 
-        request.session['cart_count'] = max(request.session.get('cart_count', 0) - quantity, 0)
-        update_cart_info(request)
+        update_cart_info(request, cart)
         return HttpResponseRedirect(reverse_lazy('view-cart'))
